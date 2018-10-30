46f581a6e7b5f75608c82ec9e050d970e126a9c1
jianbo.qin <jianbo.qin@dadaabc.com>
2018-10-30 15:16:04 +0800

    增加Model基类

---
+++ Model.php
@@ -0,0 +1,33 @@
+<?php
+
+class Model
+{
+    // 模型的table名
+    protected $tableName = '';
+    // 数据库
+    protected $db = null;
+
+    /**
+     * 指定tabel
+     *
+     * @param string $tableName
+     * @throw \Exception
+     */
+    public function setTableName(string $tableName)
+    {
+        if ($tableName == '') {
+            throw new \Exception('tableName can not be empty string');
+        }
+        $this->tableName = $tableName;
+    }
+
+    /**
+     * 获取table名
+     *
+     * @return string
+     */
+    public function getTableName() : string
+    {
+        return $this->tableName;
+    }
+}
